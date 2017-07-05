8aa06bce0302249ce29741122fce454dd5205c36
kradwan <kef3.rad@gmail.com>
2017-07-05 14:01:10 +0200

    add: init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FavoritChangedEventArgs } from './favorite/favorite.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  post = {
+    title: 'Post title',
+    isSelected: true
+  };
+  tweet = {
+    body: 'Here is the body of a tweet...',
+    likesCount: 10,
+    isLiked: false,
+  }
+  stackGrade = {
+    voteCount : 10,
+    userVote: 0
+  }
+
+  search = { url: 'https://api.spotify.com/v1/search?type=artist&q=' }
+  viewMode = 'other'; //ngSwitchCase
+
+  onFavoritChanged(args: FavoritChangedEventArgs){
+    console.log("Favorit changed!", args.newValue);
+  }
+
+  courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'}
+  ];
+
+  onAdd() {
+    this.courses.push({id: 4, name: 'course4'});
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onUpdate(course) {
+    course.name = 'update';
+  }
+
+  onVote($event) {
+    console.log('Actual vote: ', $event.userVote);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
+import { AuthorsService } from './../services/authors.service';
+import { CoursesService } from './../services/courses.service';
+import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+
+import { SummaryPipe } from './../pipes/summary.pipe';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseComponent } from './new-course/new-course.component';
+import { VoterComponent } from './voter/voter.component';
+import { SearchBoxSpotifyComponent } from './search-box-spotify/search-box-spotify.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PostsBoardComponent } from './posts-board/posts-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    PanelComponent,
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    CourseFormComponent,
+    SignupFormComponent,
+    NewCourseComponent,
+    VoterComponent,
+    SearchBoxSpotifyComponent,
+    PhotosComponent,
+    PostsBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
