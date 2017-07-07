beafac4c3297765dc81911de13d4ce8d2bb4b37d
kradwan <kef3.rad@gmail.com>
2017-07-07 22:44:36 +0200

    add: authors

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,3 +1,5 @@
+import { TemplateFormComponent } from './../shared-guards/prevent-unsaved-changes-guard.service';
+import { FormGroup } from '@angular/forms';
 import { AuthorsService } from './../../services/authors.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -6,14 +8,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
-export class AuthorsComponent implements OnInit {
-  private _authors : any[] = [];
+export class AuthorsComponent implements OnInit, TemplateFormComponent {
+  private _authors: any[] = [];
   private _title;
+  form: FormGroup;
+  // for template driven form
+  // hasUnsavedChanges(): Boolean;
 
-  constructor(private _authorsService : AuthorsService) { 
+  constructor(private _authorsService: AuthorsService) {
       console.log(_authorsService.getAll());
       this._authors = _authorsService.getAll();
-      this._title = `${this._authors.length>1? this._authors.length+" Authors":"Author"}`;      
+      this._title = `${this._authors.length > 1 ? this._authors.length + ' Authors' : 'Author'}`;
   }
 
   ngOnInit() {
